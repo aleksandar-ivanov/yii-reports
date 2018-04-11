6056a52289eafc2192d27f790066233e60c80cec
Aleksandar Ivanov <aleksandar.ivanov@mentormate.com>
2018-04-11 19:11:40 +0300

    Initial commit

---
+++ ReportsManagement.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\reports;
+
+/**
+ * reports module definition class
+ */
+class ReportsManagement extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\reports\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
