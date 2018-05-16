d0010ca1b62e824b8f50a6e51126c4e2c6c451a0
unknown <aivanov93@abv.bg>
2018-05-16 22:15:58 +0300

    FInal push

--- views/default/index.php
+++ views/default/index.php
@@ -1,12 +1,120 @@
+<?php
+
+use dosamigos\chartjs\ChartJs;
+
+?>
 <div class="reports-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+    <div class="row tile_count">
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
+            <div class="count">2500</div>
+            <span class="count_bottom"><i class="green">4% </i> From last Week</span>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
+            <div class="count">123.50</div>
+            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
+            <div class="count green">2,500</div>
+            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
+            <div class="count">4,567</div>
+            <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
+            <div class="count">2,315</div>
+            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
+            <div class="count">7,325</div>
+            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+        </div>
+    </div>
+    <div class="row">
+
+        <div class="col-md-10">
+            <?php
+            echo ChartJs::widget([
+                'type' => 'line',
+                'options' => [
+                    'height' => 400,
+                    'width' => 400
+                ],
+                'data' => [
+                    'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+                    'datasets' => [
+                        [
+                            'label' => "My First dataset",
+                            'backgroundColor' => "rgba(179,181,198,0.2)",
+                            'borderColor' => "rgba(179,181,198,1)",
+                            'pointBackgroundColor' => "rgba(179,181,198,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(179,181,198,1)",
+                            'data' => [65, 59, 90, 81, 56, 55, 40]
+                        ],
+                        [
+                            'label' => "My Second dataset",
+                            'backgroundColor' => "rgba(255,99,132,0.2)",
+                            'borderColor' => "rgba(255,99,132,1)",
+                            'pointBackgroundColor' => "rgba(255,99,132,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(255,99,132,1)",
+                            'data' => [28, 48, 40, 19, 96, 27, 100]
+                        ]
+                    ]
+                ]
+            ]);
+
+            ?>
+        </div>
+
+
+    </div>
+    <div class="row">
+        <div class="col-md-10">
+            <?php
+            echo ChartJs::widget([
+                'type' => 'bar',
+                'options' => [
+                    'height' => 400,
+                    'width' => 400
+                ],
+                'data' => [
+                    'labels' => ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh"],
+                    'datasets' => [
+                        [
+                            'label' => "Some dataset",
+                            'backgroundColor' => "rgba(179,181,198,0.2)",
+                            'borderColor' => "rgba(100,181,198,1)",
+                            'pointBackgroundColor' => "rgba(255,181,198,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(222,181,198,1)",
+                            'data' => [5, 3, 15, 44, 100, 55, 80]
+                        ],
+                        [
+                            'label' => "Different one",
+                            'backgroundColor' => "rgba(255,99,132,0.2)",
+                            'borderColor' => "rgba(255,99,132,1)",
+                            'pointBackgroundColor' => "rgba(255,99,132,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(255,99,132,1)",
+                            'data' => [33,44,55,100,25,24,60,99]
+                        ]
+                    ]
+                ]
+            ]);
+
+            ?>
+        </div>
+    </div>
 </div>
